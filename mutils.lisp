06944194ea7fa479192bc92e242e8bc5bd33adea
Mariano Montone <marianomontone@gmail.com>
2023-07-20 12:05:18 -0300

    condp

--- mutils.lisp
+++ mutils.lisp
@@ -1,4 +1,19 @@
+(defpackage :mutils
+  (:use :cl)
+  (:export :condp))
+
+(in-package :mutils)
+
 (pushnew
  (asdf:system-source-directory :mutils)
  directory-module-loader:*module-directories*)
+
+(defmacro condp (predicate &body clauses)
+  "COND using PREDICATE."
+  (let ((pred (gensym)))
+    `(let ((,pred ,predicate))
+       (cond
+	 ,@(loop for clause in clauses
+		 collect `((funcall ,pred ,(car clause))
+			   ,@(cdr clause)))))))
  
