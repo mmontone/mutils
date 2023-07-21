59d011f2a92af1c0b3aae45aad6512d5a6411cd2
Mariano Montone <marianomontone@gmail.com>
2023-07-21 11:35:30 -0300

    parse-lisp-module-file

--- mutils.lisp
+++ mutils.lisp
@@ -1,6 +1,8 @@
 (defpackage :mutils
   (:use :cl)
-  (:export :condp))
+  (:export
+   #:condp
+   #:parse-lisp-module-file))
 
 (in-package :mutils)
 
@@ -16,4 +18,61 @@
 	 ,@(loop for clause in clauses
 		 collect `((funcall ,pred ,(car clause))
 			   ,@(cdr clause)))))))
- 
+
+(defun parse-lisp-module-file (file)
+  "Parse a Lisp module file.
+
+Returns the name of the module, its short description,
+its properties (author, requirements, keywords, etc.),
+its long description/comment with instructions of usage, etc."
+  (let ((header-regex "^\\s*;;;\\s*(.*)\\s*---\\s*(.*)")
+        (commentary-regex "^\\s*;;;\\s*Commentary\\:")
+        (code-regex "^\\s*;;;\\s*Code\\:")
+        (property-regex "^\\s*;;\\s*(\\w*)\\:\\s*(.*)")
+        (commented-line-regex "^\\s*;*(.*)"))
+        
+    (with-open-file (in file)
+      ;; The first line has the module name followed by a short description.
+      (let* ((first-line (read-line in))
+             (module-name-and-desc
+               (second (multiple-value-list (ppcre:scan-to-strings header-regex first-line))))
+             (module-name nil)
+             (short-desc nil)
+             (properties nil)
+             (commentary nil)
+             (status :properties))
+        (unless module-name-and-desc
+          (error "Module should start with: ;;; <module name> --- <short description>"))
+        (setf module-name (aref module-name-and-desc 0))
+        (setf short-desc (aref module-name-and-desc 1))
+        (do ((line (read-line in nil) (read-line in nil)))
+            ((eql status :code) (values))
+          (ecase status
+            (:properties
+             ;; The following things to expect are the module properties
+             (when (ppcre:scan commentary-regex line)
+               (setf status :commentary))
+             (when (ppcre:scan code-regex line)
+               (error "Commentary section missing."))
+             (let ((property (second (multiple-value-list (ppcre:scan-to-strings property-regex line)))))
+               (when property
+                 (push (cons (aref property 0)
+                             (aref property 1))
+                       properties))))
+            (:commentary
+             (cond
+               ((ppcre:scan code-regex line)
+                (when (null commentary)
+                  (error "Commentary is empty"))
+                (setf commentary
+                      (with-output-to-string (s)
+                        (dolist (line (nreverse commentary))
+                          (write-line line s))))                          
+                (setf status :code))
+               ((ppcre:scan commented-line-regex line)
+                (push (aref (second (multiple-value-list (ppcre:scan-to-strings commented-line-regex line))) 0)
+                      commentary))))))
+        (list :name module-name
+              :description short-desc
+              :properties properties
+              :commentary commentary)))))
