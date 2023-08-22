a8d2165e480fba22da7461061ed7be8a5a527ae7
Mariano Montone <marianomontone@gmail.com>
2023-08-21 21:10:45 -0300

    lisp-critic-warnings: use declamations to control what gets critiqued

--- docs/lisp-critic-warnings.md
+++ docs/lisp-critic-warnings.md
@@ -6,16 +6,30 @@ Signal compiler warnings with lisp-critic critiques.
 
 - **Author**: Mariano Montone <marianomontone@gmail.com>
 - **Version**: 0.1
-- **Requires**: lisp-critic, compiler-hooks
+- **Requires**: lisp-critic, compiler-hooks, alexandria
 
 
  Signal compiler warnings with lisp-critic critiques.
 
  After the module is loaded, LISP-CRITIC warnings with critiques are signaled on COMPILE-FILE calls.
 
+ Use CRITIQUE declamations to control what packages, files, functions gets critiqued:
+     (declaim lisp-critic-warnings:critique nil :package)
+     (declaim lisp-critic-warnings:critique nil :file)
+     (declaim lisp-critic-warnings:critique nil my-function)
+
 
 
 ## Variables
 ### \*critic-warnings\*
 Variable for enabling or disabling Lisp critic warnings.
+
+### \*ignore-defs\*
+The list of functions to ignore when critiquing.
+
+### \*ignore-files\*
+The list of files to ignore when critiquing.
+
+### \*ignore-packages\*
+The list of packages to ignore when critiquing.
 
--- lisp-critic-warnings.lisp
+++ lisp-critic-warnings.lisp
@@ -2,12 +2,12 @@
 
 ;; Copyright (C) 2023 Mariano Montone. All rights reserved.
 
-;; This work is licensed under the terms of the MIT license.  
+;; This work is licensed under the terms of the MIT license.
 ;; For a copy, see <https://opensource.org/licenses/MIT>.
 
 ;; Author: Mariano Montone <marianomontone@gmail.com>
 ;; Version: 0.1
-;; Requires: lisp-critic, compiler-hooks
+;; Requires: lisp-critic, compiler-hooks, alexandria
 
 ;;; Commentary:
 
@@ -15,19 +15,40 @@
 ;;
 ;; After the module is loaded, LISP-CRITIC warnings with critiques are signaled on COMPILE-FILE calls.
 ;;
+;; Use CRITIQUE declamations to control what packages, files, functions gets critiqued:
+;;     (declaim lisp-critic-warnings:critique nil :package)
+;;     (declaim lisp-critic-warnings:critique nil :file)
+;;     (declaim lisp-critic-warnings:critique nil my-function)
+;;
 ;;; Code:
 
 (require :lisp-critic)
 (require :compiler-hooks)
+(require :alexandria)
 
 (defpackage :lisp-critic-warnings
   (:use :cl)
-  (:export #:*critic-warnings*))
+  (:export #:*critic-warnings*
+           #:critique ;; declamation
+           #:*ignore-packages*
+           #:*ignore-files*
+           #:*ignore-defs*))
 
 (in-package :lisp-critic-warnings)
 
+(declaim (declaration critique))
+
 (defvar *critic-warnings* t
   "Variable for enabling or disabling Lisp critic warnings.")
+
+(defvar *ignore-packages* '()
+  "The list of packages to ignore when critiquing.")
+
+(defvar *ignore-files* '()
+  "The list of files to ignore when critiquing.")
+
+(defvar *ignore-defs* '()
+  "The list of functions to ignore when critiquing.")
 
 (define-condition lisp-critic-style-warning (alexandria:simple-style-warning)
   ())
@@ -41,6 +62,18 @@
           (write-string line s)
           (terpri s))))))
 
+(defun toggle-critiques (enable? what)
+  (etypecase what
+    (package (if enable?
+                 (alexandria:removef *ignore-packages* (package-name what))
+                 (pushnew (package-name what) *ignore-packages*)))
+    (pathname (if enable?
+                  (alexandria:removef *ignore-files* what )
+                  (pushnew what *ignore-files*)))
+    (symbol (if enable?
+                (alexandria:removef *ignore-defs* what)
+                (pushnew what *ignore-defs*)))))
+
 (defun critique-file (file &rest args)
   "Critique definitions found in FILE, using patterns in NAMES."
   (declare (ignore args))
@@ -49,14 +82,29 @@
       (let ((eof (list nil)))
         (do ((code (read in nil eof) (read in nil eof)))
             ((eq code eof) (values))
-          (let ((critique
-                  (with-output-to-string (out)
-                    (lisp-critic::critique-definition code out names))))
-            (unless (zerop (length critique))
-              (setq critique (reformat-critique critique))
-              ;; TODO: the signaled condition does not contain a source code location.
-              ;; Would that be possible to add?
-              (warn 'lisp-critic-style-warning :format-control critique))))))))
+          (cond
+            ((and (eql (car code) 'declaim)
+                  (eql (cadr code) 'critique))
+             ;; a critique declamation
+             (destructuring-bind (enable? &optional (what :file)) (cdddr code)
+               (toggle-critiques enable?
+                                 (ecase what
+                                   (:file (pathname file))
+                                   (:package *package*)
+                                   (t (the symbol what))))))
+            (t
+             (unless (or (member file *ignore-files*)
+                         (member (package-name *package*) *ignore-packages*)
+                         (and (eql (car code) 'defun)
+                              (member (cadr code) *ignore-defs*)))
+               (let ((critique
+                       (with-output-to-string (out)
+                         (lisp-critic::critique-definition code out names))))
+                 (unless (zerop (length critique))
+                   (setq critique (reformat-critique critique))
+                   ;; TODO: the signaled condition does not contain a source code location.
+                   ;; Would that be possible to add?
+                   (warn 'lisp-critic-style-warning :format-control critique)))))))))))
 
 (defun maybe-critique-file (file &rest args)
   (when *critic-warnings*
