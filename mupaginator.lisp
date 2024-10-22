4120d43340d4c4159fd4c1ff847c8a4028ed17b2
Mariano Montone <marianomontone@gmail.com>
2024-10-21 21:40:26 -0300

    Paginator: another take

--- mupaginator.lisp
+++ mupaginator.lisp
@@ -18,9 +18,11 @@
 ;; (defroute pagination-test "/"
 ;;     ((page :parameter-type 'integer :init-form 1))
 ;;   (let* ((total (floor (/ (length *countries*) *page-size*)))
-;;          (pagination (mupaginator:paginate page total))
+;;          (pagination (mupaginator:make-pagination :current page
+;;                                                   :total total
+;;                                                   :page-size 10))
 ;;          (items (mapcar #'cadr
-;;                         (apply #'subseq *countries* (multiple-value-list (mupaginator:page-start-end page *page-size* (length *countries*)))))))
+;;                         (apply #'subseq *countries* (multiple-value-list (mupaginator:pagination-start-end pagination (length *countries*)))))))
 ;;     (with-html
 ;;       (:ul
 ;;        (dolist (item items)
@@ -40,7 +42,7 @@
 (defpackage :mupaginator
   (:use :cl)
   (:export #:pagination
-           #:paginate
+           #:make-pagination
            #:print-pagination
            #:print-pagination-html
            #:print-pagination-bootstrap
@@ -49,35 +51,41 @@
            #:pagination-current
            #:pagination-next
            #:pagination-prev
-           #:pagination-pages
            #:pagination-total))
 
 (in-package :mupaginator)
 
 (defstruct pagination
-  current next prev pages total)
+  (current 1 :type integer)
+  (total nil :type integer)
+  (page-size 10 :type integer))
 
-(defun paginate (current total &key (use-ellipsis t) (padding 2)
-                                 (include-first-and-last t))
-  "Create a PAGINATION structure that can be then printed to HTML.
+(defun pagination-prev (pagination)
+  (with-slots (current) pagination
+    (if (= current 1) nil (1- current))))
+
+(defun pagination-next (pagination)
+  (with-slots (current total) pagination
+    (if (= current total) nil (1+ current))))
+
+(defun pagination-buttons (pagination &key (padding 2) (use-ellipsis t)
+                                        (include-first-and-last t))
+  "Return a list of buttons for PAGINATION.
 Args:
-- CURRENT is the current page number, between 1 and TOTAL.
-- TOTAL is the total number of pages.
 - USE-ELLIPSIS: an :ELLIPSIS keyword is included when appropiate when enabled.
 - PADDING: the list of buttons has length (PADDING * 2) + 1.
-- INCLUDE-FIRST-AND-LAST: buttons for first and last page are included."  
-  (let ((prev (if (= current 1) nil (1- current)))
-        (next (if (= current total) nil (1+ current)))
-        (pages '()))
-    (when (= current total 1)
-      (return-from paginate
-        (make-pagination :current current
-                         :next next
-                         :prev prev
-                         :pages pages
-                         :total total)))
+- INCLUDE-FIRST-AND-LAST: buttons for first and last page are included."
+  (with-accessors ((current pagination-current)
+                   (total pagination-total)
+                   (prev pagination-prev)
+                   (next pagination-next))
+      pagination
 
-    (let* ((range (cond
+    (when (= current total 1)
+      (return-from pagination-buttons '(1)))
+
+    (let* ((buttons '())
+           (range (cond
                     ((< (- current padding) 1)
                      (cons 1 (+ padding padding 1)))
                     ((> (+ current padding) total)
@@ -92,41 +100,27 @@ Args:
       (when (> r1 1)
         (when use-ellipsis
           (when include-first-and-last
-            (alexandria:appendf pages '(1)))
-          (alexandria:appendf pages (list :ellipsis))))
+            (alexandria:appendf buttons '(1)))
+          (alexandria:appendf buttons (list :ellipsis))))
 
-      ;; pages slice
+      ;; buttons slice
       (loop for i from r1 to r2
-            do (alexandria:appendf pages (list i)))
+            do (alexandria:appendf buttons (list i)))
 
       (when (< r2 total)
         (when use-ellipsis
-          (alexandria:appendf pages (list :ellipsis))
+          (alexandria:appendf buttons (list :ellipsis))
 
           ;; last page
           (when include-first-and-last
-            (alexandria:appendf pages (list total)))))
+            (alexandria:appendf buttons (list total)))))
 
-      (assert (member current pages))
-      (make-pagination :current current
-                       :next next
-                       :prev prev
-                       :pages pages
-                       :total total)
-      )))
+      (assert (member current buttons))
+      buttons)))
 
-;; (paginate 1 10)
-;; (paginate 2 10)
-;; (paginate 50 100)
-;; (paginate 3 10)
-;; (paginate 4 10)
-;; (paginate 5 10)
-;; (paginate 5 10 :padding 3)
-;; (paginate 5 10 :padding 3 :use-ellipsis nil)
-
-(defun print-pagination (pagination &optional (stream *standard-output*))
+(defun print-pagination (pagination &optional (stream *standard-output*) &rest options)
   "Debug function for printing a text representation of PAGINATION."
-  (dolist (button (pagination-pages pagination))
+  (dolist (button (apply #'pagination-buttons pagination options))
     (if (eq button :ellipsis)
         (write-string "..." stream)
         (if (= button (pagination-current pagination))
@@ -140,7 +134,9 @@ Args:
                                 on-click
                                 (stream *standard-output*)
                                 (first-and-last-buttons t)
-                                (prev-and-next-buttons t))
+                                (prev-and-next-buttons t)
+                                (use-ellipsis t)
+                                (padding 2))
   "Print PAGINATION to a vanilla HTML STREAM.
 Args:
 - HREF: a FUNCTION-DESIGNATOR that is called with a page number argument and should return the URL string for that page for the buttons.
@@ -164,14 +160,17 @@ Args:
                  :onclick (when (and (pagination-prev pagination) on-click) (funcall on-click (pagination-prev pagination)))
                  :alt "Previous"
                  (who:str (who:escape-string "<")))))
-          (dolist (page (pagination-pages pagination))
-            (if (eq page :ellipsis)
+          (dolist (button (pagination-buttons pagination
+                                              :padding padding
+                                              :use-ellipsis use-ellipsis
+                                              :include-first-and-last first-and-last-buttons))
+            (if (eq button :ellipsis)
                 (who:htm (:span :class "ellipsis" (who:str "...")))
                 (who:htm
-                 (:a :class (when (= page (pagination-current pagination)) "active")
-                     :href (when href (funcall href page))
-                     :onclick (when on-click (funcall on-click page))
-                     (who:str page)))))
+                 (:a :class (when (= button (pagination-current pagination)) "active")
+                     :href (when href (funcall href button))
+                     :onclick (when on-click (funcall on-click button))
+                     (who:str button)))))
           (when prev-and-next-buttons
             (who:htm
              (:a :class (when (not (pagination-next pagination)) "disabled")
@@ -194,35 +193,23 @@ Args:
 
 #+example
 (with-output-to-string (s)
-  (print-pagination-to-html
-   (paginate 2 10)
-   (lambda (page)
-     (format nil "/page/~a" page))
+  (print-pagination-html
+   (make-pagination :current 2 :total 10)
+   :href (lambda (page)
+           (format nil "/page/~a" page))
    :stream s))
-
-;; <nav aria-label="Page navigation example">
-;;   <ul class="pagination justify-content-center">
-;;     <li class="page-item disabled">
-;;       <a class="page-link" href="#" tabindex="-1">Previous</a>
-;;     </li>
-;;     <li class="page-item"><a class="page-link" href="#">1</a></li>
-;;     <li class="page-item"><a class="page-link" href="#">2</a></li>
-;;     <li class="page-item"><a class="page-link" href="#">3</a></li>
-;;     <li class="page-item">
-;;       <a class="page-link" href="#">Next</a>
-;;     </li>
-;;   </ul>
-;; </nav>
-
-;; https://getbootstrap.com/docs/4.1/components/pagination/?
 
 (defun print-pagination-bootstrap (pagination
                                    &key
                                      href on-click
                                      (stream *standard-output*)
                                      (first-and-last-buttons t)
-                                     (prev-and-next-buttons t))
-  "Like PRINT-PAGINATION-HTML, but for Bootstrap framework."
+                                     (prev-and-next-buttons t)
+                                     (use-ellipsis t)
+                                     (padding 2))
+  "Like PRINT-PAGINATION-HTML, but for Bootstrap framework.
+
+See: https://getbootstrap.com/docs/4.1/components/pagination/"
   (who:with-html-output (html stream)
     (:ul :class "pagination"
          (when first-and-last-buttons
@@ -239,16 +226,18 @@ Args:
                      :href (when href (funcall href (pagination-prev pagination)))
                      :onclick (when on-click (funcall on-click (pagination-prev pagination)))
                      (who:str (who:escape-string "<"))))))
-         (dolist (page (pagination-pages pagination))
-           (if (eq page :ellipsis)
+         (dolist (button (pagination-buttons pagination :padding padding
+                                                        :use-ellipsis use-ellipsis
+                                                        :include-first-and-last first-and-last-buttons))
+           (if (eq button :ellipsis)
                (who:htm (:li :class "page-item disabled"
                              (:a :class "page-link" :href "#" (who:str "..."))))
                (who:htm
-                (:li :class (concatenate 'string "page-item" (if (= page (pagination-current pagination)) " active" ""))
+                (:li :class (concatenate 'string "page-item" (if (= button (pagination-current pagination)) " active" ""))
                      (:a :class "page-link"
-                         :href (when href (funcall href page))
-                         :onclick (when on-click (funcall on-click page))
-                         (who:str page))))))
+                         :href (when href (funcall href button))
+                         :onclick (when on-click (funcall on-click button))
+                         (who:str button))))))
          (when (and prev-and-next-buttons (pagination-next pagination))
            (who:htm
             (:li :class "page-item"
@@ -265,14 +254,17 @@ Args:
                      (who:str (who:escape-string ">>"))))))
          )))
 
-;; https://www.w3schools.com/w3css/w3css_pagination.asp
 (defun print-pagination-w3css (pagination
                                &key
                                  href on-click
                                  (stream *standard-output*)
                                  (first-and-last-buttons t)
-                                 (prev-and-next-buttons t))
-  "Like PRINT-PAGINATION-HTML, but for W3CSS framework."
+                                 (prev-and-next-buttons t)
+                                 (use-ellipsis t)
+                                 (padding 2))
+  "Like PRINT-PAGINATION-HTML, but for W3CSS framework.
+
+See: https://www.w3schools.com/w3css/w3css_pagination.asp"
   (who:with-html-output (html stream)
     (:div :class "w3-bar"
           (when first-and-last-buttons
@@ -287,14 +279,16 @@ Args:
                  :href (when href (funcall href (pagination-prev pagination)))
                  :onclick (when on-click (funcall on-click (pagination-prev pagination)))
                  (who:str (who:escape-string "<")))))
-          (dolist (page (pagination-pages pagination))
-            (if (eq page :ellipsis)
+          (dolist (button (pagination-buttons pagination :padding padding
+                                                         :use-ellipsis use-ellipsis
+                                                         :include-first-and-last first-and-last-buttons))
+            (if (eq button :ellipsis)
                 (who:htm (:a :class "w3-button" :href "#" (who:str "...")))
                 (who:htm
-                 (:a :class (concatenate 'string "w3-button" (if (= page (pagination-current pagination)) " w3-green" ""))
-                     :href (when href (funcall href page))
-                     :onclick (when on-click (funcall on-click page))
-                     (who:str page)))))
+                 (:a :class (concatenate 'string "w3-button" (if (= button (pagination-current pagination)) " w3-green" ""))
+                     :href (when href (funcall href button))
+                     :onclick (when on-click (funcall on-click button))
+                     (who:str button)))))
           (when (and prev-and-next-buttons (pagination-next pagination))
             (who:htm
              (:a :class "w3-button"
@@ -309,20 +303,43 @@ Args:
                  (who:str (who:escape-string ">>")))))
           )))
 
-(defun page-start-end (page page-size total)
-  "Utility function for calculating start and end for PAGE, PAGE-SIZE and TOTAL.
-Useful for getting the items of a page, given those arguments and a sequence.
+(defun pagination-start-end (pagination &optional length)
+  "Utility function for calculating start and end for PAGINATION.
+Useful for getting the items of a page for a sequence.
 
 Example usage:
-    (apply #'subseq my-seq (multiple-value-list page page-size (length my-seq)))"
-  (values (* (1- page) page-size)
-          (min (+ (* (1- page) page-size) page-size) total)))
+    (let ((pagination (make-pagination :current 2 :total 20 :page-size 10)))
+            (apply #'subseq my-seq (multiple-value-list (pagination-start-end pagination))))
+
+See: PAGINATION-SUBSEQ"
+  (with-slots (page-size current) pagination
+    (values (* (1- current) page-size)
+            (if length
+                (min (+ (* (1- current) page-size) page-size) length)
+                (+ (* (1- current) page-size) page-size)))))
+
+;; (pagination-start-end (make-pagination :current 2 :total 20 :page-size 10) 100)
+;; (pagination-start-end (make-pagination :current 3 :total 20 :page-size 10) 100)
+
+(defun pagination-subseq (pagination sequence)
+  "Get current page from SEQUENCE, according to PAGINATION."
+  (apply #'subseq sequence (multiple-value-list (pagination-start-end pagination (length sequence)))))
+
+#+example
+(let* ((p (make-pagination-for-sequence (list-all-packages))))
+  (pagination-subseq p (list-all-packages)))
+
+(defun make-pagination-for-sequence (sequence &key (current 1) (page-size 10))
+  (make-pagination :current current
+                   :page-size page-size
+                   :total (truncate (/ (length sequence) page-size))))
 
 (defun pagination-sample (total &rest args)
   (dotimes (x total)
     (let* ((page (1+ x))
-           (pagination (apply #'paginate page total args)))
-      (print-pagination pagination)
+           (pagination (make-pagination :current page
+                                        :total total)))
+      (apply #'print-pagination pagination *standard-output* args)
       (terpri))))
 
 #+test(pagination-sample 30)
