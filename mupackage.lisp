6d9aaae104678c44dcfd86a20903d4341aef8959
Mariano Montone <marianomontone@gmail.com>
2025-09-04 10:54:05 -0300

    First read-context experiment

--- mupackage.lisp
+++ mupackage.lisp
@@ -28,3 +28,42 @@
 (defmacro define-package (name &rest options)
   `(defpackage ,name
      ,@(apply #'append (mapcar #'process-option options))))
+
+
+(defvar *read-context* (make-hash-table))
+
+(defmacro define-read-context (name &rest definitions)
+  `(setf (gethash ',name *read-context*)
+         ',definitions))
+
+(defun find-read-context (name)
+  (or (gethash name *read-context*)
+      (error "Read context not found: ~s" name)))
+
+(defmacro with-read-contexts (context-designators &body body)
+  (let* ((contexts (mapcar #'find-read-context context-designators))
+         (definitions (apply #'append contexts))
+         (variables (remove-if-not (lambda (def-type)
+                                     (eql def-type :variable))
+                                   definitions
+                                   :key #'car))
+         (macros (remove-if-not (lambda (def-type)
+                                  (eql def-type :macro))
+                                definitions
+                                :key #'car))
+         (functions (remove-if-not (lambda (def-type)
+                                     (eql def-type :function))
+                                   definitions
+                                   :key #'car)))
+    `(symbol-macrolet
+         ,(loop for var in variables
+                collect (cdr var))
+       (flet
+           ,(loop for def in functions
+                  collect `(,(cadr def) (&rest args)
+                            (apply #',(caddr def) args)))
+         (macrolet
+             ,(loop for def in macros
+                    collect `(,(cadr def) (&rest args)
+                              `(,',(caddr def) ,@args)))
+           ,@body)))))
--- tests/mupackage.lisp
+++ tests/mupackage.lisp
@@ -18,4 +18,20 @@
 (define-package package-test-2
       (:use :cl)
   (:mixins :my-project-parenscript
-           :my-project-sql))
+   :my-project-sql))
+
+(define-read-context parenscript
+    (:macro ps parenscript:ps)
+  (:macro chain parenscript:chain))
+
+(with-read-contexts (parenscript)
+  (ps (chain foo bar)))
+
+(define-read-context sxql
+    (:macro select sxql:select)
+  (:macro from sxql:from)
+  (:macro where sxql:where))
+
+(with-read-contexts (sxql)
+  (select :* (from "lala")
+    (where (:= 'x 22))))
